576334874fa690fcd061366958988b339159af07
ella <lydbsdud@gmail.com>
2026-01-19 09:00:19 +0900

    chore: init doktori-backend

---
+++ src/test/java/com/example/doktoribackend/DoktoriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doktoribackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoktoriBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
