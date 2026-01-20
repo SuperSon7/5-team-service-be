8b5aaf0929b4349ce663e1f4199aeade649059f0
ella <lydbsdud@gmail.com>
2026-01-20 23:00:29 +0900

    feat: code 길이 변경

--- src/main/java/com/example/doktoribackend/user/policy/ReadingVolume.java
+++ src/main/java/com/example/doktoribackend/user/policy/ReadingVolume.java
@@ -17,7 +17,7 @@ public class ReadingVolume extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "code", nullable = false, unique = true, length = 20)
+    @Column(name = "code", nullable = false, unique = true, length = 50)
     private String code;
 
     @Column(name = "name", nullable = false, length = 50)
