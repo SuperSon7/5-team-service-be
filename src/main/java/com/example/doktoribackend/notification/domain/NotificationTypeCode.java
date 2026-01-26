1dbe708b0d15a515851b10272516ad843391b1fd
ella <lydbsdud@gmail.com>
2026-01-26 16:31:41 +0900

    feat: 알림 test 추

--- src/main/java/com/example/doktoribackend/notification/domain/NotificationTypeCode.java
+++ src/main/java/com/example/doktoribackend/notification/domain/NotificationTypeCode.java
@@ -2,7 +2,7 @@ package com.example.doktoribackend.notification.domain;
 
 public enum NotificationTypeCode {
     ROUND_START_10M_BEFORE,
-    BOOK_REVIEW_CHECKED,
-    BOOK_REVIEW_DEADLINE_24H_BEFORE,
-    BOOK_REVIEW_DEADLINE_30M_BEFORE
+    BOOK_REPORT_CHECKED,
+    BOOK_REPORT_DEADLINE_24H_BEFORE,
+    BOOK_REPORT_DEADLINE_30M_BEFORE
 }
