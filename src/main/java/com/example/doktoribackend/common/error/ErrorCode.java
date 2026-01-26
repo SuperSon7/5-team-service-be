753dc4e434d60dc7c524a1e46f6c154256dc1bbd
ella <lydbsdud@gmail.com>
2026-01-26 09:42:24 +0900

    feat: 에러 응답, 응답 매퍼 추

--- src/main/java/com/example/doktoribackend/common/error/ErrorCode.java
+++ src/main/java/com/example/doktoribackend/common/error/ErrorCode.java
@@ -63,8 +63,7 @@ public enum ErrorCode {
 
     // Notification
     NOTIFICATION_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_TYPE_NOT_FOUND", "알림 유형을 찾을 수 없습니다."),
-    NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_NOT_FOUND", "알림을 찾을 수 없습니다."),
-    FCM_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FCM_SEND_FAILED", "푸시 알림 전송에 실패했습니다.");
+    NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_NOT_FOUND", "알림을 찾을 수 없습니다.");
 
 
     private final HttpStatus status;
