065dd288200c60c1049ed842c25c9c30ea85dfb7
Superson7 <vin990227@naver.com>
2026-03-06 09:49:20 +0900

    trigger: Phase 8 selective build test (API only change)

--- api/src/main/java/com/example/doktoribackend/DoktoriBackendApplication.java
+++ api/src/main/java/com/example/doktoribackend/DoktoriBackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class DoktoriBackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Phase 8 selective build test
         SpringApplication.run(DoktoriBackendApplication.class, args);
     }
 
